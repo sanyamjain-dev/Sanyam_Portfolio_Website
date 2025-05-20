3d7713efc41e066e8d272b2f1a630afed95be183
Sanyam Jain <114089419+sanyamjain-dev@users.noreply.github.com>
2025-05-21 00:39:53 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
             <button
               onClick={() =>
                 window.open(
-                  "https://drive.google.com/file/d/1-D8PrGCZdINyhXA2eiTTnsHshhtCaeRL/view?usp=sharing",
+                  "https://drive.google.com/file/d/1l73bWSOXq8OM71tA9nmCwvpB_xqU2p4X/view?usp=sharing",
                   "_blank"
                 )
               }
